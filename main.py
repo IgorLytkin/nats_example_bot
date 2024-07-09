36b5eb651c921ac3fdbe29e7a843434d79501934
IgorLytkin <igor.lytkin.2020@ya.ru>
2024-07-09 14:35:51 +0700

    feat: delay_service (consumer.py + publisher.py), utils (start_consumers.py), user.py, config.py, main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from fluentogram import TranslatorHub
 from handlers.other import other_router
 from handlers.user import user_router
 from middlewares.i18n import TranslatorRunnerMiddleware
-from storage.nats_storage import NatsStorage
 from utils.i18n import create_translator_hub
 from utils.nats_connect import connect_to_nats
+from utils.start_consumers import start_delayed_consumer
 
 # Настраиваем базовую конфигурацию логирования
 logging.basicConfig(
@@ -30,36 +30,47 @@ async def main() -> None:
     # Загружаем конфиг в переменную config
     config: Config = load_config()
 
-    # Подключаемся к NATS
-    nc, js = await connect_to_nats(servers=config.nats.servers)
-
-    # Инициализируем хранилище на базе NATS
-    storage: NatsStorage = await NatsStorage(nc=nc, js=js).create_storage()
-
     # Инициализируем бот и диспетчер
     bot = Bot(
         token=config.tg_bot.token,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
-    dp = Dispatcher(storage=storage)
+    dp = Dispatcher()
+
+    # Подключаемся к NATS и получаем ссылки на клиент и JetStream-контекст
+    nc, js = await connect_to_nats(servers=config.nats.servers)
 
     # Создаем объект типа TranslatorHub
     translator_hub: TranslatorHub = create_translator_hub()
 
-    # Регистрируем роутеры в диспетчере
+    # Регистриуем роутеры в диспетчере
     dp.include_router(user_router)
     dp.include_router(other_router)
 
     # Регистрируем миддлварь для i18n
     dp.update.middleware(TranslatorRunnerMiddleware())
 
-    # Запускаем polling
+    # Запускаем polling и консьюмер отложенного удаления сообщений
     try:
-        await dp.start_polling(bot, _translator_hub=translator_hub)
+        await asyncio.gather(
+            dp.start_polling(
+                bot,
+                js=js,
+                delay_del_subject=config.delayed_consumer.subject,
+                _translator_hub=translator_hub
+            ),
+            start_delayed_consumer(
+                nc=nc,
+                js=js,
+                bot=bot,
+                subject=config.delayed_consumer.subject,
+                stream=config.delayed_consumer.stream,
+                durable_name=config.delayed_consumer.durable_name
+            )
+        )
     except Exception as e:
         logger.exception(e)
     finally:
-        # Закрываем соединение с NATS
         await nc.close()
         logger.info('Connection to NATS closed')
 
